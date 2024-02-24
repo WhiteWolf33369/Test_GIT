87d989b6000bb7882259050886691e451a96e264
Ash <alisherburiev333@gmail.com>
2024-02-24 16:46:25 +0500

    Create module parcins_sites

---
+++ First/Parcing_sites.py
@@ -0,0 +1,20 @@
+import requests  #Импортируем модуль
+
+#создаем класс виртуальных сайтов
+class Virtualsite:
+    #Стандартная функцция Init
+    def __init__(self, name):
+        self.name = name
+    #создаем запрос на сайт, st_accept- желаемый формат получения данных, st_useragent - данные браузера,
+    #с которого отправляется запрос. page- страница сайта(В нынешнем случае валюта)
+    def request(self, st_accept = "text/html", st_useragent, src="", page = ""):
+        self.headers = {"Accept": st_accept,
+                        "User-Agent": st_useragent}
+        src1 = src+"/"+page
+        req = requests.get(src1, self.headers)
+        return req
+    #Получение значения нужных переменных
+    def get_count(self, *variable):
+        ...
+__name__ = '__main__'
+
