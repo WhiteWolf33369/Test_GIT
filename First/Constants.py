52e499c8daf8ee9fb32ace2deccd306eedfcfdb9
Ash <alisherburiev333@gmail.com>
2024-02-25 15:45:30 +0500

    Create module parcins_sites Create file Constants.py

--- First/Constants.py
+++ First/Constants.py
@@ -6,3 +6,7 @@ BRAUSERS = {'''Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHT
             /120.0.0.0 Safari/537.36""",
             """Mozilla/5.0 (Linux; Android 13; M2012K11AG) AppleWebKit/537.36 (KHTML, like Gecko) Chrome
             /120/0/6099/230 Mobile Safari/537.36 OPR/80.2.4244.77484"""}
+SITES = {}
+CITIES = {}
+FOREX = {}
+DISTRICTS = {}
