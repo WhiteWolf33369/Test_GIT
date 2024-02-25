4917147ac49530caacfd2769e186fed51b0a7bf8
Ash <alisherburiev333@gmail.com>
2024-02-25 15:48:29 +0500

    Create module parcins_sites Create file Constants.py

--- First/Parcing_sites.py
+++ First/Parcing_sites.py
@@ -1,20 +1,25 @@
-import requests  #Импортируем модуль
+# Импортируем модуль
+import requests
 
-#создаем класс виртуальных сайтов
+
+# создаем класс виртуальных сайтов
 class Virtualsite:
-    #Стандартная функцция Init
+    # Стандартная функцция Init
     def __init__(self, name):
         self.name = name
-    #создаем запрос на сайт, st_accept- желаемый формат получения данных, st_useragent - данные браузера,
-    #с которого отправляется запрос. page- страница сайта(В нынешнем случае валюта)
-    def request(self, st_accept = "text/html", st_useragent, src="", page = ""):
+
+    # создаем запрос на сайт, st_accept- желаемый формат получения данных, st_useragent - данные браузера,
+    # с которого отправляется запрос. page- страница сайта(В нынешнем случае валюта)
+    def request(self, st_accept="text/html", st_useragent="", src="", page=""):
         self.headers = {"Accept": st_accept,
                         "User-Agent": st_useragent}
-        src1 = src+"/"+page
+        src1 = src + "/" + page
         req = requests.get(src1, self.headers)
         return req
-    #Получение значения нужных переменных
+
+    # Получение значения нужных переменных
     def get_count(self, *variable):
         ...
-__name__ = '__main__'
 
+
+__name__ = '__main__'
